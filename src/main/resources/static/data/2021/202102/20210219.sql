a2dbf43d9853044730860ac7f5fde92be995d74c
Jhonatan Garcia <jhonatan.garcia@colombiansofture.com>
2021-02-22 21:32:40 -0500

    Corregir archivo

--- src/main/resources/static/data/2021/202102/20210219.sql
+++ src/main/resources/static/data/2021/202102/20210219.sql
@@ -4,4 +4,4 @@ COMMENT ON TABLE usuario_usrp IS '2021-02-19';
 INSERT INTO propiedadvalordefinido_pvdp (cpvd_llave, cpvd_origen, cpvd_nombre, cpvd_codigo, cpvd_grupo, bpvd_textoculto, bpvd_solicitamotivo) 
 	VALUES('PROP_160' , 'L', 'CAMPO RENDER ESPECIAL', 'PLANTILLA_RENDER_ESPECIAL_SQL', 'REQUISITO', true, true);
 INSERT INTO propiedadvalordefinido_pvdp (cpvd_llave, cpvd_origen, cpvd_nombre, cpvd_codigo, cpvd_grupo, bpvd_propiedadboolean, bpvd_piderol) 
-	VALUES('PROP_161' , 'L', 'CAMPO RENDER ESPECIAL', 'PLANTILLA_RENDER_ESPECIAL_SQL', 'REQUISITO',true, true);
+	VALUES('PROP_161' , 'L', 'TRANSFERIR', 'PERMISO_PLANTILLA_TRANSFERIR', 'REQUISITO',true, true);
