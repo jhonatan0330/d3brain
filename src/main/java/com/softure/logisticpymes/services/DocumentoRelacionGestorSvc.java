f35f32bd93c1c8ef5d9ae8fb61a1301b08f23e6e
Jhonatan Garcia <jhonatan.garcia@colombiansofture.com>
2021-06-11 13:06:41 -0500

    Corregir trazabilidad de procesos automaticos

--- src/main/java/com/softure/logisticpymes/services/DocumentoRelacionGestorSvc.java
+++ src/main/java/com/softure/logisticpymes/services/DocumentoRelacionGestorSvc.java
@@ -94,7 +94,7 @@ public class DocumentoRelacionGestorSvc extends BasicSvc<DocumentoRelacionGestor
 			if(dto.getEstado().length() > 2 && dto.getEstado().charAt(2) == '1') verMensaje = "1";
 			if(dto.getEstado().length() > 3 && dto.getEstado().charAt(3) == '1') verInventarios = "1";
 			if(dto.getEstado().length() > 4 && dto.getEstado().charAt(4) == '1') verReportes = "1";
-			if(dto.getEstado().length() > 5 && dto.getEstado().charAt(5) == '1') usuarioAutomatico = documentoRelacionGestorMapper.getSystemUser();
+			if(dto.getEstado().length() > 5 && dto.getEstado().charAt(5) != '1') usuarioAutomatico = documentoRelacionGestorMapper.getSystemUser();
 			if(dto.getEstado().length() > 6 && dto.getEstado().charAt(6) == '1') verApi = "1";
 		}
 		return documentoRelacionGestorMapper.listarExpedientesGestionadores(
