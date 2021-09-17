78b1de4a4ef3ab1c184fe4e311f3da8ec11629e3
Jhonatan Garcia <jhonatan.garcia@colombiansofture.com>
2021-09-17 00:14:44 -0500

    NO validar el total cuando se modifique el campo

--- src/main/java/com/softure/logisticpymes/services/adapter/TipoNumero.java
+++ src/main/java/com/softure/logisticpymes/services/adapter/TipoNumero.java
@@ -41,7 +41,8 @@ public class TipoNumero {
 				if(pCampo.getModificado()) {
 					//Valido que del cliente este ien calculado
 					if((pCampo.getLlaveTabla()==null && Propiedades.obtenerParametro(pCampo.getCampoDTO(), Propiedades.PERMISO_CAMPO_BLOQUEAR)!=null)
-							|| (pCampo.getLlaveTabla()!=null && Propiedades.obtenerParametro(pCampo.getCampoDTO(), Propiedades.PERMISO_CAMPO_MODIFICABLE)!=null)){
+							|| (pCampo.getLlaveTabla()!=null && Propiedades.obtenerParametro(pCampo.getCampoDTO(), Propiedades.PERMISO_CAMPO_MODIFICABLE)==null)){
+						// En tcm se tienen permisos de modificar el seguro
 						BigDecimal valorCalculado = calcular(pCampo);
 						if(pCampo.getValorNumero().compareTo(valorCalculado)!=0) 
 							throw new ServerException("El campo " + pCampo.getCampoDTO().getNombre() + " no se calculo correctamente, valor esperado : (" + SoftureUtil.formatMoney(valorCalculado) +  ") y se recibe (" + SoftureUtil.formatMoney(pCampo.getValorNumero()) +")");
