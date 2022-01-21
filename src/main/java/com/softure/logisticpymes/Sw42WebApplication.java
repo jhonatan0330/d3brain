bbcf4f6c8ebb1fd26c4c82fc5c73844180c910a6
Jhonatan Garcia <jhonatan.garcia@colombiansofture.com>
2022-01-21 15:49:29 -0500

    Intento 2 de hacer funcionar en jar los jpa

--- src/main/java/com/softure/logisticpymes/Sw42WebApplication.java
+++ src/main/java/com/softure/logisticpymes/Sw42WebApplication.java
@@ -1,5 +1,6 @@
 package com.softure.logisticpymes;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.text.ParseException;
@@ -37,7 +38,9 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 import com.softure.java.dto.exception.ServerException;
 import com.softure.logisticpymes.services.MensajeSvc;
@@ -262,7 +265,6 @@ public class Sw42WebApplication  extends SpringBootServletInitializer implements
 	
 	
 	//Soporta que la SPA de angular funcione con solo el jar
-	/*
 	@Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
@@ -276,5 +278,5 @@ public class Sw42WebApplication  extends SpringBootServletInitializer implements
                                 : new ClassPathResource("/static/index.html");
                     }
                 });
-    }*/
+    }
 }
