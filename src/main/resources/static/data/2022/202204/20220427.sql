c7a0fe48c348561dcf95e0312dd1450f74290206
Jhonatan Garcia <jhonatan.garcia@colombiansofture.com>
2022-04-27 17:13:40 -0500

    Propiedad MAximo numero

---
+++ src/main/resources/static/data/2022/202204/20220427.sql
@@ -0,0 +1,4 @@
+COMMENT ON TABLE usuario_usrp IS '2022-04-27';
+
+INSERT INTO propiedadvalordefinido_pvdp (cpvd_llave, cpvd_origen, cpvd_nombre, cpvd_codigo, cpvd_grupo, cpvd_origencategoria, bpvd_textoculto) 
+	VALUES('PROP_201' , 'C', 'NUMERO MAXIMO', 'NUMERO_MAXIMO', 'REQUISITO', 'N', true);
--- src/main/resources/static/data/3. Initial_Properties.sql
+++ src/main/resources/static/data/3. Initial_Properties.sql
@@ -373,3 +373,5 @@ INSERT INTO propiedadvalordefinido_pvdp (cpvd_llave, cpvd_origen, cpvd_nombre, c
 	VALUES('PROP_199' , 'C', 'ANULAR MOVIMIENTO', 'CUENTA_ANULAR_MOVIMIENTO', 'REQUISITO', 'Z', true);
 INSERT INTO propiedadvalordefinido_pvdp (cpvd_llave, cpvd_origen, cpvd_nombre, cpvd_codigo, cpvd_grupo, cpvd_origencategoria, bpvd_textoculto) 
 	VALUES('PROP_200' , 'C', 'DETALLE OCULTAR UNIDADES NOMBRE CANTIDAD', 'DETALLE_OCULTAR_UNIDADES_NOMBRE_CANTIDAD', 'REQUISITO', 'J', true);
+INSERT INTO propiedadvalordefinido_pvdp (cpvd_llave, cpvd_origen, cpvd_nombre, cpvd_codigo, cpvd_grupo, cpvd_origencategoria, bpvd_textoculto) 
+	VALUES('PROP_201' , 'C', 'NUMERO MAXIMO', 'NUMERO_MAXIMO', 'REQUISITO', 'N', true);
