378f68acc865f9cec6dbf86f106a44b5b02500aa
Jhonatan Garcia <jhonatan.garcia@colombiansofture.com>
2022-08-12 12:48:51 -0500

    corregir funcion

--- src/main/resources/static/data/2022/202208/20220811.sql
+++ src/main/resources/static/data/2022/202208/20220811.sql
@@ -3,7 +3,7 @@ COMMENT ON TABLE usuario_usrp IS '2022-08-11';
 CREATE OR REPLACE FUNCTION public.ultima_gestion(_documento character varying, _estado character varying)
  RETURNS TABLE(cdrg_llave character varying, cdrg_documentoprincipal character varying, cdrg_documentomodificador character varying, ddrg_fecha timestamp with time zone, cdrg_estadoinicial character varying, cdrg_estadofinal character varying, cdrg_estado character varying, cdrg_ubicacion character varying, cdrg_valores character varying, cdrg_usuario character varying, ddrg_cierre timestamp with time zone, cdrg_nombre character varying, cdrg_transaccion character varying)
  LANGUAGE plpgsql
-AS $function$
+AS '
 begin
 	return query select 
 		drg.cdrg_llave,
@@ -20,8 +20,7 @@ begin
 		drg.cdrg_nombre,
 		drg.cdrg_transaccion
 	from documentorelaciongestor_drgp drg 
-	where drg.cdrg_documentoprincipal = _documento and drg.cdrg_estado = 'A' and drg.cdrg_estadofinal = _estado
+	where drg.cdrg_documentoprincipal = _documento and drg.cdrg_estado = ''A'' and drg.cdrg_estadofinal = _estado
 	order by drg.ddrg_fecha desc
 	limit 1;	
-END;$function$
-;
+END;';
