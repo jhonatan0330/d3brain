e34f9830a1a7aaf1820c0a195b75060d00a8c287
Jhonatan Garcia <jhonatan.garcia@colombiansofture.com>
2023-02-21 08:52:53 -0500

    Corregir el nombre de la columna de tareas

--- src/main/resources/static/data/logisticpymes/Postgres8.sql
+++ src/main/resources/static/data/logisticpymes/Postgres8.sql
@@ -893,7 +893,7 @@ CREATE TABLE task_task_tsk (
         ctsk_title character varying(200) NOT NULL,
         ctsk_notes character varying(4000),
         dtsk_completed timestamp with time zone,
-        dtsk_due timestamp with time zone,
+        dtsk_duedate timestamp with time zone,
         ntsk_priority int4 NOT NULL DEFAULT 0,
         ntsk_order int4 NOT NULL DEFAULT 0,
         dtsk_createdAt timestamp with time zone  NOT NULL,
