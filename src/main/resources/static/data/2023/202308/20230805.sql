40a89fc0cea08266188eb6fea0338cbf0c3c5591
Jhonatan Garcia <jhonatan.garcia@colombiansofture.com>
2023-08-05 16:59:48 -0500

    Propiedad Instrucciones de plantilla corregida

---
+++ src/main/resources/static/data/2023/202308/20230805.sql
@@ -0,0 +1,3 @@
+COMMENT ON TABLE usuario_usrp IS '2023-08-05';
+
+update propiedadvalordefinido_pvdp set bpvd_textoculto = true, bpvd_propiedadboolean= false where cpvd_llave= 'PROP_190';
--- src/main/resources/static/data/logisticpymes/3. Initial_Properties.sql
+++ src/main/resources/static/data/logisticpymes/3. Initial_Properties.sql
@@ -351,7 +351,7 @@ INSERT INTO propiedadvalordefinido_pvdp (cpvd_llave, cpvd_origen, cpvd_nombre, c
 	VALUES('PROP_188' , 'C', 'CAMPO INVISIBLE U OCULTO', 'INVISIBLE', 'REQUISITO', true, true);
 INSERT INTO propiedadvalordefinido_pvdp (cpvd_llave, cpvd_origen, cpvd_nombre, cpvd_codigo, cpvd_grupo, bpvd_pidefechas, bpvd_solicitamotivo, bpvd_pideusuario) 
 	VALUES('PROP_189' , 'L', 'TEMPORIZADOR', 'TEMPORIZADOR', 'REQUISITO', true, true, true);
-INSERT INTO propiedadvalordefinido_pvdp (cpvd_llave, cpvd_origen, cpvd_nombre, cpvd_codigo, cpvd_grupo, bpvd_propiedadboolean) 
+INSERT INTO propiedadvalordefinido_pvdp (cpvd_llave, cpvd_origen, cpvd_nombre, cpvd_codigo, cpvd_grupo, bpvd_textoculto) 
 	VALUES('PROP_190' , 'L', 'PLANTILLA_INSTRUCCION_CREAR', 'PLANTILLA_INSTRUCCION_CREAR', 'REQUISITO', true);
 INSERT INTO propiedadvalordefinido_pvdp (cpvd_llave, cpvd_origen, cpvd_nombre, cpvd_codigo, cpvd_grupo, bpvd_multiple, bpvd_textoculto) 
 	VALUES('PROP_191' , 'W', 'API_VALIDATION', 'API_VALIDATION', 'REQUISITO', true, true);
