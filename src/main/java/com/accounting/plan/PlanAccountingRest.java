c268bb4b13944c2306be2dfeb3941e905c94bd43
Jhonatan Garcia <jhonatan.garcia@colombiansofture.com>
2024-05-07 10:28:49 -0500

    Cors

--- src/main/java/com/accounting/plan/PlanAccountingRest.java
+++ src/main/java/com/accounting/plan/PlanAccountingRest.java
@@ -3,6 +3,7 @@ package com.accounting.plan;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import com.accounting.plan.domain.ResultMapDTO;
 import com.shared.domain.ServerException;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("acc/plan")
 public class PlanAccountingRest {
 	
