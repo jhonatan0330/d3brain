bff2e697fde2303f10493e28096905383d8f320b
Jhonatan Garcia <jhonatan.garcia@colombiansofture.com>
2025-02-28 15:19:51 -0500

    Corregir error de AES

--- src/main/java/com/softure/report/application/ReporteBaseSvc.java
+++ src/main/java/com/softure/report/application/ReporteBaseSvc.java
@@ -31,7 +31,6 @@ import com.softure.document_execution.application.PedidoVentaSvc;
 import com.softure.document_execution.application.field.Propiedades;
 import com.softure.document_execution.domain.PedidoVentaCaracteristicaDTO;
 import com.softure.document_execution.domain.PedidoVentaDTO;
-import com.softure.java.services.SoftureUtil;
 
 import javax.sql.DataSource;
 
@@ -332,7 +331,6 @@ public class ReporteBaseSvc extends BasicSvc<ReporteBaseDTO, ReporteBaseFilterDT
 			if (key != null)
 				parametrosJasper.putAll(llenarParametros(key));
 			parametrosJasper.putAll(parametrosPropiedades(reporte, usuario));
-			parametrosJasper.put("P_AUTHENTICITY_CODE", SoftureUtil.encrypt(reporte.getLlaveTabla() + ejecucion.getFechaInicio().toString(), "SW42"));
 			String tipoReporte = (String) parametrosJasper.get("P_JASPERTIPO");
 			Object propiedadExcel = null;
 			// Seccion del reporte
