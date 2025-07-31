9cab5f02b1cb7d1653f234f5c1dd0a2537f4c8f7
Jhonatan garcia <jhonatan.garcia@colombiansofture.com>
2025-07-31 17:43:06 -0500

    Binario NO

--- src/main/java/com/softure/document_execution/application/field/TipoBinario.java
+++ src/main/java/com/softure/document_execution/application/field/TipoBinario.java
@@ -17,9 +17,7 @@ public class TipoBinario {
 	public void validarPrepararCampo(PedidoVentaCaracteristicaDTO pCampo, String token, boolean isUpdateAutomatic) throws ServerException{
 		if(pCampo.getValorNumero()==null || pCampo.getValorNumero().compareTo(BigDecimal.ZERO)==0){
 			String parametro = Propiedades.obtenerValor(pCampo.getCampoDTO(), Propiedades.BINARIO_FALSO);
-			if( parametro.isEmpty()){
-				pCampo.setValorText("NO");
-			}else{
+			if( !parametro.isEmpty()){
 				pCampo.setValorText(parametro);	
 			}
 		}else{
